bdeb704e7b8b4404fbd4ef2e095b34f82daf24c3
Kuilic <109625322+kaneki-code@users.noreply.github.com>
2025-03-24 19:13:31 +0000

    almost done

--- index.js
+++ index.js
@@ -26,22 +26,41 @@ const Country = mongoose.model("Country", countrySchema, "Countries");
 
 // Create a POST route for "/add/country" that adds a country using the request body (3 points)
 // Use postman to add at least THREE different countries
-
+app.post("/add/country", async (req,res)=>{
+  const country = await new Country({
+    country: req.body.country,
+    flagURL: req.body.flagURL,
+    population: req.body.population,
+    officialLanguage: req.body.officialLanguage,
+    hasNuclearWeapons: req.body.hasNuclearWeapons
+  }).save()
+  res.json(country)
+})
 
 // Create a GET route for "/" that renders countries.ejs with every country from the Countries collection (1 point)
-
+app.get("/", async(req,res)=>{
+  const data = await Country.find({})
+  res.render("home.ejs", {data})
+})
 
 // Go to countries.ejs and follow the tasks there (2 points)
 
 
 // Create a dynamic PATCH route handler for "/update/{name}" that modifies the population of the country specified in the path (3 points)
 // Test this route on post man
-
+app.patch("/info/:path", async(req,res)=>{
+  const path = req.params.path
+  const data = await Country.findOneAndUpdate({name: path})
+  res.json(data)
+})
 
 
 // Create a DELETE route handler for "/delete/country" that deletes a country of your choice (3 points)
 // Test this route on post man
-
+app.delete("/delete/:country", async (req,res)=>{
+  const response = await Country.findByIdAndDelete({country:req.params.name})
+  res.json(response)
+})
 
 async function startServer() {
   
